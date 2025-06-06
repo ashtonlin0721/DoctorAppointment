5c819c5cdadbf5ed4367b6add23f9c9adb8d0b8f
Ashton Lin <markke@Marks-MacBook-Air-4.local>
2025-06-06 19:03:16 +0800

    added appointment page and component, and also success to send appointment to database

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import CreateDoctor from './pages/CreateDoctor'
 import { useRoutes } from 'react-router-dom'
 import DoctorListPage from './pages/DoctorListPage'
 import BookAppointment from './pages/BookAppointment'
+import AppointmentPage from './pages/AppointmentPage'
 
 function App() {
   let element = useRoutes([
     {path:'/createdoctor', element: <CreateDoctor />},
     {path: '/doctorlist', element: <DoctorListPage />},
-    {path: '/book-appointment/:id', element: <BookAppointment />}
+    {path: '/book-appointment/:id', element: <BookAppointment />},
+    {path: '/appointment/:id', element: <AppointmentPage />}
   ])
 
   return (
