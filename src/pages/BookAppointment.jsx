66eaeec438bb88f0004c4ed7992c2f740f8ecc4f
Ashton Lin <markke@Marks-MacBook-Air-4.local>
2025-06-07 16:54:24 +0800

    set up to get uid from localstorage to book appointment

--- src/pages/BookAppointment.jsx
+++ src/pages/BookAppointment.jsx
@@ -11,6 +11,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
 import dayjs from 'dayjs';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 
 
@@ -21,6 +22,7 @@ export default function BookAppointment() {
   const [date, setDate] = useState(dayjs());
   const [bookedSlots = [], setBookedSlots] = useState([]);
   const [problem, setProblem] = useState("");
+  const [user, setUser] = useLocalStorage('user', null);
 
 
 
@@ -86,7 +88,7 @@ export default function BookAppointment() {
     const newDay = dayjs(date).format('YYYY-MM-DD')
     const appointment = {
       doctorId: doctor.id,
-      patientId: 'VGYHycwkzES0GJwNUIKHm0OtRuH2',
+      patientId: user?.uid,
       slot: selectedSlot,
       date: newDay,
       doctorName: doctor.firstName + " " + doctor.lastName,
