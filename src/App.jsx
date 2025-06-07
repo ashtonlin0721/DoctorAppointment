0fde8342927113a0f2436738801bbb63a3e8177d
Ashton Lin <markke@Marks-MacBook-Air-4.local>
2025-06-07 14:50:27 +0800

    added signin page, and set up localstorage and success to store data in localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,16 @@ import { useRoutes } from 'react-router-dom'
 import DoctorListPage from './pages/DoctorListPage'
 import BookAppointment from './pages/BookAppointment'
 import AppointmentPage from './pages/AppointmentPage'
+import SignInPage from './pages/Signin'
+import Home from './pages/Home'
 
 function App() {
   let element = useRoutes([
     {path:'/createdoctor', element: <CreateDoctor />},
     {path: '/doctorlist', element: <DoctorListPage />},
     {path: '/book-appointment/:id', element: <BookAppointment />},
-    {path: '/appointment/:id', element: <AppointmentPage />}
+    {path: '/appointment/:id', element: <AppointmentPage />},
+    {path: '/signin', element: <SignInPage />}
   ])
 
   return (
