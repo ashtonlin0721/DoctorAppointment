301146ae178c6d4587d73284f6f5968e75476e6d
Ashton Lin <markke@Marks-MacBook-Air-4.local>
2025-06-08 17:38:20 +0800

    useed appWrapper to wrap all routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import CreateDoctor from './pages/CreateDoctor'
-import { useRoutes } from 'react-router-dom'
+import { Route, useLocation, Routes } from 'react-router-dom'
 import DoctorListPage from './pages/DoctorListPage'
 import BookAppointment from './pages/BookAppointment'
 import AppointmentPage from './pages/AppointmentPage'
@@ -9,23 +9,84 @@ import SignInPage from './pages/Signin'
 import Home from './pages/Home'
 import MyAppointmentPage from './pages/MyAppointmentPage'
 import Header from './components/Header'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { adminAccountId } from './config/config'
+import { Navigate } from 'react-router-dom'
 
 function App() {
+  const userInfo = JSON.parse(localStorage.getItem('user'));
+  const isLoggedIn = userInfo !== null;
+  const isAdmin = isLoggedIn && userInfo.uid === adminAccountId;
 
-  let element = useRoutes([
-    {path: '/', element: <Home /> },
-    {path:'/createdoctor', element: <CreateDoctor />},
-    {path: '/doctorlist', element: <DoctorListPage />},
-    {path: '/book-appointment/:id', element: <BookAppointment />},
-    {path: '/appointment/:id', element: <AppointmentPage />},
-    {path: '/signin', element: <SignInPage />},
-    {path: '/myappointments', element: <MyAppointmentPage />},
-  ])
+
+  const AppWrapper = ()=> {
+    const location = useLocation();
+    const shouldDisplayHeader = location.pathname !== '/signin' && location.pathname !== '/register' && location.pathname !== '/resetpassword';
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+      navigate('/');
+    }, []);
+
+    return(
+    <>
+      {shouldDisplayHeader && <Header />}
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/signin" element={<SignInPage />} />
+          <Route
+            path="/createdoctor"
+            element={isAdmin ? <CreateDoctor /> : <Navigate to="/" />}
+          />
+          <Route
+            path="doctorlist"
+            element={isLoggedIn ? <DoctorListPage /> : <Navigate to="/" />}
+            
+          />
+
+          <Route
+            path='alldoctors'
+            element={isLoggedIn ? <DoctorListPage /> : <Navigate to="/" />}
+
+          />
+          
+          <Route
+            path="/book-appointment/:id"
+            element={isLoggedIn ? <BookAppointment /> : <Navigate to="/" />
+            }
+          />
+          
+          <Route
+            path="/appointment/:id"
+            element={isLoggedIn ? <AppointmentPage /> : <Navigate to="/" />
+            }
+          />
+
+          <Route
+            path="/myappointments"
+            element={isLoggedIn ? <MyAppointmentPage /> : <Navigate to="/" />
+            }
+          />
+
+
+
+        </Routes>
+
+
+
+    </>
+
+  
+    )
+  }
+
+  
 
   return (
     <>
-
-      {element}
+      <AppWrapper />
     </>
   )
 }
